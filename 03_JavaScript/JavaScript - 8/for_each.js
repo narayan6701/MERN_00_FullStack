fdb1de5381b376b6b46b134fafc80b78a2ff20bc
Narayan Bhardwaj <narayan6701@outlook.in>
2024-08-30 11:59:31 +0530

    for_each.js edited

--- 03_JavaScript/JavaScript - 8/for_each.js
+++ 03_JavaScript/JavaScript - 8/for_each.js
@@ -1,22 +1,19 @@
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 
-let print = function (el)
-{
-    console.log(el);
+let print = function (el) {
+  console.log(el);
 };
 
 arr.forEach(print);
 
 console.log("By defining a function");
 
-arr.forEach(function (el)
-{
-    console.log(el);
+arr.forEach(function (el) {
+  console.log(el);
 });
 
 console.log("By defining a arrow function");
 
-arr.forEach((el)=>
-{
-    console.log(el);
+arr.forEach((el) => {
+  console.log(el);
 });
