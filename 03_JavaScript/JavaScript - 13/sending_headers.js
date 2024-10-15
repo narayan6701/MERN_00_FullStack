c3c40f58a58aa16652a1aec9e1d1ddc2c14edea8
Narayan Bhardwaj <narayan6701@outlook.in>
2024-10-16 03:21:33 +0530

    New HTML concept and JS

--- 03_JavaScript/JavaScript - 13/sending_headers.js
+++ 03_JavaScript/JavaScript - 13/sending_headers.js
@@ -1,2 +1,11 @@
 const joke = "https://icanhazdadjoke.com";
 
+async function getJoke() {
+    try{
+        const config={headers:{Accept:"application/json"}};
+        let res =  await axios.get(joke, config);
+        console.log(res);
+    }catch(e){
+        console.log(e);
+    } 
+}
